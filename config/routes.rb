688764d4a738ab5f47931ca3b2ed98d78ff7f0a1
Igor Ustinov <dentalon599@gmail.com>
2018-12-12 18:59:32 +0300

    Добавлена интернационализация

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks/text/:id' => 'tasks#get_text'
-  root 'tasks#index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'tasks/text/:id' => 'tasks#get_text'
+    root 'tasks#index'
 
-  resources :tasks
+    resources :tasks
 
-  patch '/done/:id', to: 'tasks#done', as: :done_task
+    patch '/done/:id', to: 'tasks#done', as: :done_task
+  end
 end
